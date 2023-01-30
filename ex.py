df1fd51bd89670e4c4e8d6e5d2db92335c111c26
b0st0n6 <123112796+b0st0n6@users.noreply.github.com>
2023-01-30 11:26:25 +0600

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,136 @@
+# 2
+import random
+# l1 = ['ab', 18, 36, 'qwerty', 55]
+
+
+
+
+# 3.
+# список - ['f', 5, 785]
+# множество - {'f', 5, 785}
+# множество нельзя вычеслить по индексу.
+
+
+
+# 4
+# список - ['f', 5, 785]
+# кортеж - ('f', 5, 785)
+
+# кортежи не изменяется
+
+
+##5
+# def
+
+
+#6
+# с помошью len()
+# l = [4, 4, 10, 1]
+# print(len(l))
+
+
+##7.
+#
+# x = 15
+# x = str(x)
+# x = int(x)
+
+
+###8
+# l1 = ['ab', 18, 36, 'qwerty', 55]
+# print(*l1)
+
+
+###9.
+# int, float, str, bool, list, dict, tuple, set
+
+##10
+# def square(num1, num2):
+#     result = (num1 + num2) ** 2
+#     return result
+#
+#
+# print(square(2, 2))
+
+
+###11
+# def myList(x):
+#     l = []
+#     for i in x:
+#         l.append(i)
+#     print(l)
+#
+#
+# myList('boston')
+
+
+# 12
+# from random import randint
+# a = 'qwertyuiopasdfghjklzxcvbnm'
+#
+# c = []
+# for i in range(5):
+#     index = random.randint(0, 25)
+#     b = a[index]
+#     c.append(b)
+# print(c)
+
+
+###13
+# stroke = 'I love programming'
+#
+# for i in stroke:
+#     print(i)
+
+
+##14
+# from math import pi
+#
+# print(pi)
+
+
+####15
+# number = int(input())
+# b = 0
+# for i in range(1, number + 1):
+#     if number % i == 0:
+#         b += 1
+#
+#
+# if b == 2 or number == 1:
+#     print('yes')
+# else:
+#     print('no')
+
+
+#16. if, elif, else
+
+
+#17.
+# int = 15
+# №"@@!& = 15
+# 1num = 15
+
+
+
+#18.
+# a = 5
+# del a
+# print(a)
+
+
+
+#19
+# множество = {'gdgdgdg', 55, 88}
+# словарь = {5: 15, 'a': 'b'}
+
+
+
+
+#20
+# file = open('output.txt', 'w')
+# for i in range(1000):
+#     if i % 3 == 0 and i % 5 == 0:
+#         file.write(str(i) + '\n')
+
+
